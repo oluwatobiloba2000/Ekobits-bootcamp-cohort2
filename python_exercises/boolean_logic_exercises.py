52cc5f886438296180dd31948b5e10a5e2899303
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-12-04 00:09:01 +0100

    solved python first exercise

---
+++ python_exercises/boolean_logic_exercises.py
@@ -0,0 +1,42 @@
+
+first = "Hello World"
+
+# This is a comment
+
+print("I am a computer")
+
+if 1 < 2 and 4 > 2:
+    print("Maths is fun")
+
+# Assign a variable called nope to an absence of value.
+nope = None
+
+print("true" and "false")
+
+string_length = "What is my length?"
+print(len(string_length))
+
+shout = "I am shouting"
+
+print(shout.upper())
+print(str(4) + 'real')
+print(3 * 'cool')
+# print( 1 / 0)
+print(type([]))
+
+name = input('what is your name? ');
+
+num = int(input('choose a number '))
+if num < 0:
+    print('That number is less than 0!')
+elif num > 0:
+    print('That number is greater than 0!')
+else:
+    print('You picked 0!')
+
+print("apple".find('l'))
+
+print("y" in "xylophone")
+
+my_string = 'my string'
+(print(my_string.islower()))
